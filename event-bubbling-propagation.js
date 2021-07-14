1ba70f7641dd26a438cd194c01ca892dbaa979d2
@reeeham <reeehamgamal@gmail.com>
2021-07-15 00:02:12 +0200

    event bubbling and propagation

---
+++ event-bubbling-propagation.js
@@ -0,0 +1,30 @@
+
+//element.addEventListener(type, func, useCapture)
+let m = document.getElementById('m');
+let d = document.getElementById('d');
+let p = document.getElementById('p');
+let s = document.getElementById('s');
+let log = console.log;
+
+let highlight = (ev) =>{
+   //add css class "gold" to the clicked element
+   ev.stopPropagation();
+   let target = ev.currentTarget;
+   target.className = 'gold';
+   reset(target);
+}
+
+let reset = (_element) => {
+    setTimeout(()=>{
+        _element.className = '';
+    },3000)
+}
+
+d.addEventListener('click',(ev)=>{
+    ev.stopImmediatePropagation();
+    log('Hi I\'m a DIV');
+});
+
+[m,d,p,s].forEach((element )=> {
+    element.addEventListener('click',highlight)
+});
